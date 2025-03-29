Fix fisheye distortion calculation (#39)
- computation was incorrect
- a few minor tweak to fix warnings

Co-authored-by: Qi Wu <qiwu@nvidia.com>

threedgrut/datasets/dataset_colmap.py
@@ -159,8 +159,8 @@ class ColmapDataset(Dataset, BoundedMultiViewDataset, DatasetVisualization):
             rays_o_cam = torch.zeros_like(rays_d_cam)
             return (
                 params.to_dict(),
-                torch.tensor(rays_o_cam, dtype=torch.float32, device=self.device).reshape(out_shape),
-                torch.tensor(rays_d_cam, dtype=torch.float32, device=self.device).reshape(out_shape),
+                rays_o_cam.to(torch.float32).reshape(out_shape),
+                rays_d_cam.to(torch.float32).reshape(out_shape),
                 type(params).__name__,
             )
 

threedgrut/trainer.py
@@ -141,7 +141,7 @@ class Trainer3DGRUT:
             batch_size=1,
             shuffle=True,
             pin_memory=True,
-            persistent_workers=True,
+            persistent_workers=True if conf.num_workers > 0 else False,
         )
         val_dataloader = torch.utils.data.DataLoader(
             val_dataset,
@@ -149,7 +149,7 @@ class Trainer3DGRUT:
             batch_size=1,
             shuffle=False,
             pin_memory=True,
-            persistent_workers=True,
+            persistent_workers=True if conf.num_workers > 0 else False,
         )
         self.train_dataset = train_dataset
         self.train_dataloader = train_dataloader